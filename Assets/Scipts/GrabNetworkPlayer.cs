8f9e9448345b27d67b4cb03f08a54863be8ec8a7
Lucas Severo Alves <lucassalves65@gmail.com>
2020-01-01 16:21:49 -0300

    correct reference baing passed

--- Assets/Scipts/GrabNetworkPlayer.cs
+++ Assets/Scipts/GrabNetworkPlayer.cs
@@ -1,12 +1,18 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+//using Cinemachine;
 
 public class GrabNetworkPlayer : MonoBehaviour
 {
     // Start is called before the first frame update
+    private GameObject generatedPlayer;
+
     void Start()
     {
+        //generatedPlayer = GameObject.Find("/Player(Clone)");
+        //var vcam = GetComponent<CinemachineVirtualCamera>();
+
         
     }
 
---
+++ Assets/Scipts/Player.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using Cinemachine;
+
+public class Player : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    [SerializeField]
+    private string PlayerId = "";
+
+    void Start()
+    {
+        // get plyaer id from server/services
+        this.PlayerId = "base_player";
+
+        GameObject vCam = GameObject.FindGameObjectsWithTag("VirtualCamera")[0];
+        var vcam = vCam.GetComponent<CinemachineVirtualCamera>();
+        var currentPlayer = this.gameObject.transform;
+        if (vcam != null){
+            print("camera aq");
+
+        }
+        if (currentPlayer!= null){
+            print("player aq");
+
+        }
+        vcam.LookAt = currentPlayer;
+        vcam.Follow = currentPlayer;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
